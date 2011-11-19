c50e506f5242e91a72b5bcb0404a5d159f5b3d1e
Jamie Palmer <jjiceman2007@gmail.com>
2011-11-19 13:56:47 -0500

    somewhat working tracking

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -30,58 +30,87 @@ namespace KinectTest
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             myRuntime = Runtime.Kinects[0];
-            myRuntime.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking);
+            myRuntime.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking | RuntimeOptions.UseColor);
+            myRuntime.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
             myRuntime.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.DepthAndPlayerIndex);
-            myRuntime.SkeletonFrameReady += new EventHandler <SkeletonFrameReadyEventArgs>(Depth_Ready);
+            myRuntime.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(nui_SkeletonFrameReady);
+            myRuntime.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(Video_Ready);
         }
 
-        void Depth_Ready(object sender, SkeletonFrameReadyEventArgs e)
+        void Video_Ready(object sender, ImageFrameReadyEventArgs e)
         {
-            preview.Children.Clear();
-            SkeletonData data = e.SkeletonFrame.Skeletons[0];
+            PlanarImage Image = e.ImageFrame.Image;
+            preview.Background = (new ImageBrush(BitmapSource.Create(
+                Image.Width, Image.Height, 96, 96, PixelFormats.Bgr32, null, Image.Bits, Image.Width * 4)));
+        }
 
+        private void moveCircle(SkeletonData data)
+        {
             double x = data.Joints[JointID.HandRight].Position.X;
             double y = data.Joints[JointID.HandRight].Position.Y;
             double z = data.Joints[JointID.HandRight].Position.Z;
-
-            preview.Children.Add(getBodySegment(data.Joints, Brushes.Green, JointID.HipCenter, JointID.Spine, JointID.ShoulderCenter, JointID.Head));
-            preview.Children.Add(getBodySegment(data.Joints, Brushes.Green, JointID.ShoulderCenter, JointID.ShoulderLeft, JointID.ElbowLeft, JointID.WristLeft, JointID.HandLeft));
-            preview.Children.Add(getBodySegment(data.Joints, Brushes.Green, JointID.ShoulderCenter, JointID.ShoulderRight, JointID.ElbowRight, JointID.WristRight, JointID.HandRight));
-            preview.Children.Add(getBodySegment(data.Joints, Brushes.Green, JointID.HipCenter, JointID.HipLeft, JointID.KneeLeft, JointID.AnkleLeft, JointID.FootLeft));
-            preview.Children.Add(getBodySegment(data.Joints, Brushes.Green, JointID.HipCenter, JointID.HipRight, JointID.KneeRight, JointID.AnkleRight, JointID.FootRight));
-
-            /*
             double width = canvas.ActualWidth;
             double height = canvas.ActualHeight;
 
-            double dispX = width * x + width / 2.0;
-            double dispY = height * y + height / 2.0;
+            double dispX = width * x / 2.0 + width / 2.0;
+            double dispY = height * y / 2.0 - height / 2.0;
 
             textBlock1.Text = "X: " + x + "\nY: " + y + "\nZ: " + z + "\nDX: " + dispX + "\nDY: " + dispY;
 
             Canvas.SetLeft(pointer, dispX);
             Canvas.SetTop(pointer, dispY);
-            */
+
         }
 
-
-
-        // MS WORKER METHODS
-
-        private Point getDisplayPosition(Joint joint)
+        // Thanks Microsoft Samples (don't sue me):
+        private void nui_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
         {
-            // Worker method provided by Microsoft, converts a Joint to a Point used on the canvas
-            float depthX, depthY;
-            myRuntime.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
-            depthX = Math.Max(0, Math.Min(depthX * 320, 320));  //convert to 320, 240 space
-            depthY = Math.Max(0, Math.Min(depthY * 240, 240));  //convert to 320, 240 space
+            SkeletonFrame skeletonFrame = e.SkeletonFrame;
 
-            return new Point((int)(depthX), (int)(depthY));
+            //KinectSDK TODO: this shouldn't be needed, but if power is removed from the Kinect, you may still get an event here, but skeletonFrame will be null.
+            if (skeletonFrame == null)
+            {
+                return;
+            }
+
+            int iSkeleton = 0;
+            Brush[] brushes = new Brush[1];
+            brushes[0] = new SolidColorBrush(Colors.Red);
+            brushes[0].Opacity = 0.5;
+
+            preview.Children.Clear();
+            foreach (SkeletonData data in skeletonFrame.Skeletons)
+            {
+                if (SkeletonTrackingState.Tracked == data.TrackingState)
+                {
+                    moveCircle(data);
+                    // Draw bones
+                    Brush brush = brushes[iSkeleton % brushes.Length];
+                    preview.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.Spine, JointID.ShoulderCenter, JointID.Head));
+                    preview.Children.Add(getBodySegment(data.Joints, brush, JointID.ShoulderCenter, JointID.ShoulderLeft, JointID.ElbowLeft, JointID.WristLeft, JointID.HandLeft));
+                    preview.Children.Add(getBodySegment(data.Joints, brush, JointID.ShoulderCenter, JointID.ShoulderRight, JointID.ElbowRight, JointID.WristRight, JointID.HandRight));
+                    preview.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.HipLeft, JointID.KneeLeft, JointID.AnkleLeft, JointID.FootLeft));
+                    preview.Children.Add(getBodySegment(data.Joints, brush, JointID.HipCenter, JointID.HipRight, JointID.KneeRight, JointID.AnkleRight, JointID.FootRight));
+
+                    // Draw joints
+                    foreach (Joint joint in data.Joints)
+                    {
+                        Point jointPos = getDisplayPosition(joint);
+                        Line jointLine = new Line();
+                        jointLine.X1 = jointPos.X - 3;
+                        jointLine.X2 = jointLine.X1 + 6;
+                        jointLine.Y1 = jointLine.Y2 = jointPos.Y;
+                        jointLine.Stroke = Brushes.Red;
+                        jointLine.StrokeThickness = 4;
+                        preview.Children.Add(jointLine);
+                    }
+                }
+                iSkeleton++;
+            } // for each skeleton
         }
 
-        Polyline getBodySegment(JointsCollection joints, Brush brush, params JointID[] ids)
+        private Polyline getBodySegment(Microsoft.Research.Kinect.Nui.JointsCollection joints, Brush brush, params JointID[] ids)
         {
-            // Combines given points into a line segment
             PointCollection points = new PointCollection(ids.Length);
             for (int i = 0; i < ids.Length; ++i)
             {
@@ -93,6 +122,21 @@ namespace KinectTest
             polyline.Stroke = brush;
             polyline.StrokeThickness = 5;
             return polyline;
+        }
+
+        private Point getDisplayPosition(Joint joint)
+        {
+            float depthX, depthY;
+            myRuntime.SkeletonEngine.SkeletonToDepthImage(joint.Position, out depthX, out depthY);
+            depthX = depthX * 320; //convert to 320, 240 space
+            depthY = depthY * 240; //convert to 320, 240 space
+            int colorX, colorY;
+            ImageViewArea iv = new ImageViewArea();
+            // only ImageResolution.Resolution640x480 is supported at this point
+            myRuntime.NuiCamera.GetColorPixelCoordinatesFromDepthPixel(ImageResolution.Resolution640x480, iv, (int)depthX, (int)depthY, (short)0, out colorX, out colorY);
+
+            // map back to skeleton.Width & skeleton.Height
+            return new Point((int)(preview.Width * colorX / 640.0), (int)(preview.Height * colorY / 480));
         }
 
         private void slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
